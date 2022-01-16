b57fff5feb4ae47d7d4550e5812837b742aab23c
Newt42 <59628325+Newt4217@users.noreply.github.com>
2022-01-16 18:29:23 +0100

    Update client.py

--- client.py
+++ client.py
@@ -1,6 +1,7 @@
 # bot.py
 import os
 import discord
+import random
 
 from dotenv import load_dotenv
 
@@ -12,5 +13,23 @@ client = discord.Client()
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    test_collection = [
+        '1',
+        '2',
+        (
+            '3.1, '
+            '3.2'
+        ),
+    ]
+
+    if message.content == 'test_collection':
+        response = random.choice(test_collection)
+        await message.channel.send(response)
 
 client.run(TOKEN)
