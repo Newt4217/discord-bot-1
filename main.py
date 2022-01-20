c7865882bfe404b06bbf3b57ab18a2e652472cd9
Newt42 <59628325+Newt4217@users.noreply.github.com>
2022-01-20 12:51:43 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,13 @@ import json
 import requests
 
 
-parameters = {
-    "ids": "bitcoin",
-    "vs_currencies": "usd"
-}
-
-response = requests.get("https://api.coingecko.com/api/v3/simple/price", params=parameters)
-print(response.json())
+#parameters = {
+#    "ids": "bitcoin",
+#    "vs_currencies": "usd"
+#}
+def price_calc(ids: str, vs_currencies: str)
+    parameters = { 'ids': ids,
+                   'vs_currencies': vs_currencies
+                 }
+    response = requests.get("https://api.coingecko.com/api/v3/simple/price", params=parameters)
+    return response.json()
