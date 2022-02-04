df7387c4c9d0694909e8ef79eac7df9c8d4c2cc6
Newt42 <59628325+Newt4217@users.noreply.github.com>
2022-02-05 00:40:13 +0100

    Update main.py
    removed pre-defined params

--- main.py
+++ main.py
@@ -2,10 +2,6 @@ import json
 import requests
 
 
-#parameters = {
-#    "ids": "bitcoin",
-#    "vs_currencies": "usd"
-#}
 def price_calc(ids: str, vs_currencies: str):
     parameters = { 'ids': ids,
                    'vs_currencies': vs_currencies
