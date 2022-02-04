5199e18c8238b5989907784852d54808419789af
Newt42 <59628325+Newt4217@users.noreply.github.com>
2022-02-05 00:32:29 +0100

    Update and rename test.py to mimcrvratioasync.py
    etherscan API key turned into an env variable
    renamed ratio_calc 
    added return to ratio_calc lmao

--- mimcrvratioasync.py
+++ mimcrvratioasync.py
@@ -1,6 +1,11 @@
+import os
 import json
 import requests
 import asyncio
+from dotenv import load_dotenv
+
+load_dotenv()
+APIKEY = os.getenv('ETHERSCAN_API')
 
 async def mim_calc():
     parameters_mim = { 'module': 'account',
@@ -8,7 +13,7 @@ async def mim_calc():
                    'contractaddress': '0x99d8a9c45b2eca8864373a26d1459e3dff1e17f3',
                    'address': '0x5a6A4D54456819380173272A5E8E9B9904BdF41B',
                    'tag': 'latest',
-                   'apikey': 'HWN3T1MVI2MEFS7Z4ZFFBZCYZGVK2S7II2'
+                   'apikey': APIKEY
                  }
     response_mim = requests.get("https://api.etherscan.io/api", params=parameters_mim)
     mimamt = response_mim.json()['result']
@@ -20,19 +25,11 @@ async def crv_calc():
                    'contractaddress': '0x6c3F90f043a72FA612cbac8115EE7e52BDe6E490',
                    'address': '0x5a6A4D54456819380173272A5E8E9B9904BdF41B',
                    'tag': 'latest',
-                   'apikey': 'HWN3T1MVI2MEFS7Z4ZFFBZCYZGVK2S7II2'
+                   'apikey': APIKEY
                  }
     response_crv = requests.get("https://api.etherscan.io/api", params=parameters_crv)
     crvamt = response_crv.json()['result']
     return crvamt
 
-async def calc():
-    print((int(await mim_calc())*(10**-18))/(int(await crv_calc())*(10**-18)))
-
-if __name__ == "__main__":
-    import time
-    s = time.perf_counter()
-    asyncio.run(calc())
-    elapsed = time.perf_counter() - s
-    print(f"{__file__} executed in {elapsed:0.2f} seconds.")
-
+async def ratio_calc():
+    return (int(await mim_calc())*(10**-18))/(int(await crv_calc())*(10**-18))
